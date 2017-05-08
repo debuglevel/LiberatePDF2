4b2900e94ec1270e5dda71090c6b9f63079102a5
Marc Kohaupt <marc.kohaupt@gmail.com>
2017-05-08 21:51:54 +0200

    Cleanup

--- LiberatePDF2-Rest-Service/src/main/java/rocks/huwi/liberatepdf2/restservice/restrictionsremover/RestrictionRemoveController.java
+++ LiberatePDF2-Rest-Service/src/main/java/rocks/huwi/liberatepdf2/restservice/restrictionsremover/RestrictionRemoveController.java
@@ -16,8 +16,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponents;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -57,19 +55,22 @@ public class RestrictionRemoveController {
 			// no item found with this ID (because no request was assigned this
 			// ID by now)
 			log.debug("No document with ID={} found", documentId);
-			
-			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(String.format("No document found for ID=%s", documentId));
+
+			return ResponseEntity.status(HttpStatus.NOT_FOUND)
+					.body(String.format("No document found for ID=%s", documentId));
 		} else if (pdf.isDone() == false) {
 			// the request exists, but was not transformed by now
 			log.debug("Document with ID={} found, but pdf.isDone=false (not processed by now)", documentId);
-			
-			return ResponseEntity.status(HTTP_STATUS_IN_PROGRESS).body("The document was not processed by now. Please try again later.");
+
+			return ResponseEntity.status(HTTP_STATUS_IN_PROGRESS)
+					.body("The document was not processed by now. Please try again later.");
 		} else if (Files.exists(pdf.getUnrectrictedPath()) == false) {
 			// the request was transformed, but the file does not exist (somehow
 			// failed?)
 			log.debug("Document with ID={} found, but no file exists", documentId);
 
-			return ResponseEntity.status(HTTP_STATUS_FAILED).body("The document was processed, but produced no result. Maybe the password was wrong or another error occurred.");
+			return ResponseEntity.status(HTTP_STATUS_FAILED).body(
+					"The document was processed, but produced no result. Maybe the password was wrong or another error occurred.");
 		} else {
 			// request should be okay
 			final String filename = this.storageService.getItem(documentId).getOriginalFilename();
@@ -77,20 +78,24 @@ public class RestrictionRemoveController {
 
 			log.debug("Document with ID={} found and set for delivery", documentId);
 
-			return ResponseEntity.ok().contentType(MediaType.APPLICATION_PDF).header("Content-Disposition", "attachment; filename=\"" + filename + "\"").body(filesystemResource);
+			return ResponseEntity.ok().contentType(MediaType.APPLICATION_PDF)
+					.header("Content-Disposition", "attachment; filename=\"" + filename + "\"")
+					.body(filesystemResource);
 		}
 	}
 
 	@RequestMapping(method = RequestMethod.POST, value = "/")
-	public ResponseEntity<?> uploadAndRemoveRestrictions(final PdfDTO restrictedPdf, UriComponentsBuilder uriComponentsBuilder) {
+	public ResponseEntity<?> uploadAndRemoveRestrictions(final PdfDTO restrictedPdf,
+			final UriComponentsBuilder uriComponentsBuilder) {
 		log.debug("Received POST request for document {}", restrictedPdf.getFile().getName());
 
 		final Pdf pdf = this.storageService.store(restrictedPdf.getFile());
 		pdf.setPassword(restrictedPdf.getPassword());
 
 		this.restrictionsRemoverService.removeRestrictionsAsync(pdf);
-		
-		UriComponents uriComponents = uriComponentsBuilder.path("/api/v1/documents/{id}").buildAndExpand(pdf.getId());
+
+		final UriComponents uriComponents = uriComponentsBuilder.path("/api/v1/documents/{id}")
+				.buildAndExpand(pdf.getId());
 		return ResponseEntity.accepted().location(uriComponents.toUri()).body(pdf.getId());
 	}
 }
