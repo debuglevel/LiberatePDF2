1cb741e1837fb627aaf548a3e7cd4be142635834
Marc Kohaupt <debuglevel@gmail.com>
2021-01-07 21:01:03 +0100

    Add command to generate Kotlin code

--- generate-openapi-libraries.sh
+++ generate-openapi-libraries.sh
@@ -1,1 +1,2 @@
 docker run --rm -v ${PWD}:/local openapitools/openapi-generator-cli generate -i /local/microservice/build/tmp/kapt3/classes/main/META-INF/swagger/liberatepdf2-microservice-0.1.yml -g java -o /local/openapi/java --additional-properties=apiPackage=de.debuglevel.liberatepdf2.api
+docker run --rm -v ${PWD}:/local openapitools/openapi-generator-cli generate -i /local/microservice/build/tmp/kapt3/classes/main/META-INF/swagger/liberatepdf2-microservice-0.1.yml -g kotlin -o /local/openapi/kotlin --additional-properties=packageName=de.debuglevel.liberatepdf2.client
