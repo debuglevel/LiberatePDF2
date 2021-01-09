a7305a33ac5c172b36d7a999f699adb212df51ff
Marc Kohaupt <debuglevel@gmail.com>
2021-01-09 12:27:17 +0100

    Prepare kotlin-client directory for branched code generation

--- kotlin-client/src/main/kotlin/de/debuglevel/liberatepdf2/client/infrastructure/RequestMethod.kt
+++
@@ -1,8 +0,0 @@
-package de.debuglevel.liberatepdf2.client.infrastructure
-
-/**
- * Provides enumerated HTTP verbs
- */
-enum class RequestMethod {
-    GET, DELETE, HEAD, OPTIONS, PATCH, POST, PUT
-}
