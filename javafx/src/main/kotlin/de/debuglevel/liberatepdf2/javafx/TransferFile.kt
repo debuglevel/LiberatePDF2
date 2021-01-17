e9cd8ad5ea1e7a6caa270ae8e032aba4266ba3a1
Marc Kohaupt <debuglevel@gmail.com>
2021-01-17 19:36:15 +0100

    Move fields into ctor

--- javafx/src/main/kotlin/de/debuglevel/liberatepdf2/javafx/TransferFile.kt
+++ javafx/src/main/kotlin/de/debuglevel/liberatepdf2/javafx/TransferFile.kt
@@ -6,10 +6,9 @@ class TransferFile(
     var path: Path,
     var status: String,
     var password: String? = null,
+    var done: Boolean = false,
+    var id: String? = null,
 ) {
-    var done = false
-    var id: String? = null
-
     override fun toString(): String {
         return "[$status] ${path.fileName}"
     }
